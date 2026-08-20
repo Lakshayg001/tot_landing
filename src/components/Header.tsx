c8a1fed459b2c90008fbf4133de4af28527d01e8
Lakshayg001 <lakshaygoel2431@gmail.com>
2026-08-20 18:22:40 +0530

    Update logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
     <header className="w-full pt-4 pb-0 px-6 md:px-12 flex justify-between items-center max-w-7xl mx-auto">
       <div className="flex items-center -ml-4">
         <a href="/" className="flex items-center">
-          <img src={logo} alt="TOT - The Only Teacher" className="h-32 md:h-48 w-auto object-contain scale-110 origin-left" />
+          <img src={logo} alt="TOT - The Only Teacher" className="h-28 md:h-40 w-auto object-contain origin-left" />
         </a>
       </div>
     </header>
